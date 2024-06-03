63c638d75ec1031ed62c89f7e8ce6fd68eff43a3
Aubrey <141216701+aubrxyy@users.noreply.github.com>
2024-06-04 03:18:14 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        DATABASE_URL: 'postgresql://kel9:ZclOT0vh1pzk@ep-withered-waterfall-a14grkmm.ap-southeast-1.aws.neon.tech/kel9basdat?sslmode=require'
     },
 }
 
