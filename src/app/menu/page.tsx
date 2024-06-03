48cbf6a9e636f737ac06f60cf7cd57eec7ff5029
aubrey <mantiriaubrey@apps.ipb.ac.id>
2024-06-04 01:06:41 +0700

    capek

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,261 @@
+import Hero from '../_components/Hero';
+import '../css/globals.css';
+
+export default function Menu() {
+    return (
+        <>
+            <Hero bgImage="/daftarmenu2.jpg" pageTitle="Daftar Menu" pageLink="/menu"/>
+            <section className="dfmenu" id="dfmenu">
+                <h2>Daftar Menu</h2>
+                <h3>Menu Nasi Pecel Ayam</h3>
+    
+                <div className="row">
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#" className="item-detail-button"
+                            ><i data-feather="eye"></i
+                            ></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasitutug.jpg" alt="Menu 1" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Paket 1</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#" className="item-detail-button"
+                            ><i data-feather="eye"></i
+                            ></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasikuning.jpg" alt="Menu 2" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Paket 2</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#" className="item-detail-button"
+                            ><i data-feather="eye"></i
+                            ></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasitutug.jpg" alt="Menu 3" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Paket 3</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                </div>
+    
+                <h3>Menu Nasi Tutug Oncom</h3>
+    
+                <div className="row">
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#"><i data-feather="eye"></i></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasitutug.jpg" alt="Menu 1" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Mantap</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#"><i data-feather="eye"></i></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasikuning.jpg" alt="Menu 2" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Inpari</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#"><i data-feather="eye"></i></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasitutug.jpg" alt="Menu 3" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Baroma</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#"><i data-feather="eye"></i></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasitutug.jpg" alt="Menu 4" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Sintanur</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp500.000</div>
+                        </div>
+                    </div>
+                </div>
+    
+                <h3>Menu Nasi Liwet</h3>
+    
+                <div className="row">
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#"><i data-feather="eye"></i></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasitutug.jpg" alt="Menu 1" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Agro 1</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#"><i data-feather="eye"></i></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasikuning.jpg" alt="Menu 2" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Agro 2</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#"><i data-feather="eye"></i></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasitutug.jpg" alt="Menu 3" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Agro 3</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#"><i data-feather="eye"></i></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasitutug.jpg" alt="Menu 4" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Agro 4</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp300.000</div>
+                        </div>
+                    </div>
+                    <div className="menu-card">
+                        <div className="menu-icon">
+                            <a href="#"><i data-feather="eye"></i></a>
+                        </div>
+                        <div>
+                            <img src="/daftarmenu/nasitutug.jpg" alt="Menu 4" />
+                        </div>
+                        <div className="menu-content">
+                            <h4>Agro 5</h4>
+                            <div className="menu-stars">
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star" className="star"></i>
+                                <i data-feather="star"></i>
+                                <i data-feather="star"></i>
+                            </div>
+                            <div className="product-price">Rp500.000</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
