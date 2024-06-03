7f9ab94e1f3bd97f4a9794277ae6c432bdeec8b8
aubrey <mantiriaubrey@apps.ipb.ac.id>
2024-06-04 00:07:44 +0700

    capek

--- src/app/paket-wisata/page.tsx
+++ src/app/paket-wisata/page.tsx
@@ -42,13 +42,13 @@ const ColumnsPage = () => {
 
         <section className='pb-28 pt-16 bg-white'>
             <div className="mx-36">
-                <div className='flex flex-wrap items-center gap-16 justify-center 2xl:justify-center'>
+                <div className='flex flex-wrap items-center gap-x-16 gap-y-10 justify-center'>
                 {tourPackages.map((pkg) => (
-                    <div key={pkg.id_paket} className='shadow-lg max-lg:w-[60rem] w-96 lg:min-h-[35rem] p-4 rounded-xl transition-all'>
+                    <div key={pkg.id_paket} className='shadow-md max-lg:w-[60rem] w-96 lg:min-h-[27rem] p-4 rounded-xl'>
                         <h1 className={`${poppins.className} text-center my-4 text-xl`}>{pkg.nama_paket}</h1>
                         <div className="bg-orange-400 h-[1px] w-full mb-4"></div>
                         <h1 className={`${poppins.className} text-left my-4 text-md/ w-[25ch]`}>{pkg.deskripsi_harga}</h1>
-                        <p dangerouslySetInnerHTML={{ __html: pkg.deskripsi.replace(/\n/g, '<br /><br />') }} className='mb-3 text-justify text-gray-500'></p>
+                        <p dangerouslySetInnerHTML={{ __html: pkg.deskripsi.replace(/\n/g, '<br />') }} className='mb-3 text-sm/loose text-justify text-gray-500'></p>
                     </div>
                 ))}
                 </div>
